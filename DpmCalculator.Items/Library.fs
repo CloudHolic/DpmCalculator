c9ac255e799ba5abdf49b1e5d5dfefd07c4e41e4
CloudHolic <bluegkstjr@kaist.ac.kr>
2022-11-23 17:28:09 +0900

    Add project files.

---
+++ DpmCalculator.Items/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace DpmCalculator.Items
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ DpmCalculator/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace DpmCalculator
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
