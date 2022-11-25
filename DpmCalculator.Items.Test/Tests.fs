74a263732982615f999fb05909985f7bc5729bd7
CloudHolic <bluegkstjr@kaist.ac.kr>
2022-11-25 17:27:14 +0900

    Parse SetData.json

--- DpmCalculator.Items.Test/Tests.fs
+++ DpmCalculator.Items.Test/Tests.fs
@@ -5,13 +5,23 @@ module Tests =
     open Xunit
     open FSharp.Json
     open DpmCalculator.Items.Item
+    open DpmCalculator.Items.SetEffect
 
     [<Fact>]
-    let ``My test`` () =
+    let ParseItemDataTest () =
         let data =
             @"C:\Projects\DpmCalculator\DpmCalculator.Items\Data\ItemData.json"
             |> File.ReadAllText
             |> Json.deserialize<ItemBase list>
             |> List.filter (fun x -> Option.isSome x.Job)
+
+        printfn "%A" data
+
+    [<Fact>]
+    let ParseSetDataTest () =
+        let data =
+            @"C:\Projects\DpmCalculator\DpmCalculator.Items\Data\SetData.json"
+            |> File.ReadAllText
+            |> Json.deserialize<SetEffectBase list>
 
         printfn "%A" data
